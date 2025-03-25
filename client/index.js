5970778a842fb9d61ad0f0ac03ab154cec61b3ef
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-03-25 17:56:13 +0100

    add sorteggio

--- client/index.js
+++ client/index.js
@@ -1,9 +1,10 @@
 const list = document.getElementById('list')
+let names = []
 
 axios
   .get(`http://localhost:3000/api/names`)
   .then(res => {
-    const names = res.data
+    names = res.data
     for (let i = 0; i < names.length; i++) {
 
       const li = document.createElement('li')
@@ -13,5 +14,20 @@ axios
   }
   )
   .catch(err => console.error(err))
+
+
+const button = document.createElement('button')
+button.textContent = 'Sorteggia'
+list.append(button)
+
+button.addEventListener('click', () => random(names, list))
+
+function random(array, nodo) {
+  let element = array[Math.floor(Math.random() * array.length)]
+  console.log(element)
+  const p = document.createElement('p')
+  p.textContent = element
+  return nodo.appendChild(p)
+}
 
 
